ec5b8611ea66d5059c13ad9d001585845da81f9e
arist0v <arist0v@disroot.org>
2022-02-21 11:30:52 -0500

    add requirements.txt, and create class with init

--- pkg/hq_mqtt_deamon.py
+++ pkg/hq_mqtt_deamon.py
@@ -1,3 +1,9 @@
 """mqtt daemon for webtio-hydroqc-addon for webthings gateway"""
 
 from ..res.hydroqc2mqtt.daemon import Hydroqc2Mqtt
+
+class hq_mqtt_deamon(Hydroqc2Mqtt):
+    """mqtt daemon class"""
+
+    def __init__(self):
+        super().__init__()
