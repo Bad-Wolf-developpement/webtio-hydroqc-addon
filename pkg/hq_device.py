ac512981c17fce77ca0880407d4d459a398de07e
arist0v <arist0v@disroot.org>
2022-02-24 15:25:23 -0500

    forget ()

--- pkg/hq_device.py
+++ pkg/hq_device.py
@@ -37,7 +37,7 @@ class hq_Device(Device):
         self._webuser = WebUser(config['username'], config['password'], False)
         #self.name = 'Hydro Quebec Winter Credit Event 3'#not sure where it's used
         self.init_session()
-        self.get_user_info
+        self.get_user_info()
         print(self._webuser.first_name)
 
     def init_session(self):
