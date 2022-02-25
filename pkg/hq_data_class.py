2c1cd17de4099809e48156a3f8aad1ed1c58d6cc
arist0v <arist0v@disroot.org>
2022-02-25 12:57:44 -0500

    data class added

---
+++ pkg/hq_data_class.py
@@ -0,0 +1,11 @@
+""" Data class for configuration data"""
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class hq_Datas:
+
+    lastSync: datetime
+    nextEvent: datetime
+    credit: float
+    #eventTable#TODO: create a table to store multiple next event and not just one
