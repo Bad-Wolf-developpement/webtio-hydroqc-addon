663a8e4f4e6e512f010069b1fbdb73ab6f22fa26
arist0v <arist0v@disroot.org>
2022-02-28 15:09:53 -0500

    debug

--- pkg/hq_device.py
+++ pkg/hq_device.py
@@ -72,7 +72,7 @@ class hq_Device(Device):
         """
         update datas if changed
         """
-        if self.adapter.verobse:
+        if self.adapter.verbose:
             print("Before pull: {0}",format(self.datas.lastSync))
         self.pull_data()
         if self.adapter.verbose:
