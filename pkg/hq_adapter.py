2871c20fc78ca2d2bc4a312567fff7317a5225e9
arist0v <arist0v@disroot.org>
2022-03-09 15:55:41 -0500

    debug

--- pkg/hq_adapter.py
+++ pkg/hq_adapter.py
@@ -36,7 +36,7 @@ class hq_Adapter(Adapter):
 
         self.pairing=False
         self.start_pairing(_TIMEOUT)
-        print(self.get_device('hydroqc-maison').find_property('ActiveEvent').set_cached_value(True))
+        print(self.get_device('hydroqc-maison').find_property('ActiveEvent').set_cached_value_and_notify(True))
         #self.async_main()
 
     def start_pairing(self, timeout):
