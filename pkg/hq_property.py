68b484287d6f93297612a2a178f9c86faa2cffd0
arist0v <arist0v@disroot.org>
2022-03-10 13:33:20 -0500

    debug

--- pkg/hq_property.py
+++ pkg/hq_property.py
@@ -69,8 +69,8 @@ class hq_bool_ro_property(hqProperty):
             return False
     
     def update(self, newValue):
-        self.set_cached_value_and_notify(newValue)
-        #elf.device.
+        self.set_cached_value(newValue)
+        self.device.notify_property_changed(self)
 
 class hq_float_ro_property(hqProperty):
     """int property, read only"""
