023aa49bbce9d46a6aaf2ba168cef865aa6071e5
arist0v <arist0v@disroot.org>
2022-03-11 09:37:09 -0500

    debug

--- pkg/hq_adapter.py
+++ pkg/hq_adapter.py
@@ -80,14 +80,14 @@ class hq_Adapter(Adapter):
             print("Starting Loops")
 
         small_loop = asyncio.new_event_loop()
-        t = Thread(target=self.start_loop, args=(small_loop,))
+        t = Thread(target=self.small_loop)
         t.start()
 
         big_loop = asyncio.new_event_loop()
         t = Thread(target=self.start_loop, args=(big_loop,))
         t.start()
 
-        asyncio.run_coroutine_threadsafe(self.small_loop(), small_loop)
+        #asyncio.run_coroutine_threadsafe(self.small_loop(), small_loop)
         asyncio.run_coroutine_threadsafe(self.big_loop(), big_loop)
 
     async def small_loop(self):
