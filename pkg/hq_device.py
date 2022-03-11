a8389209d98973d12caee0c39b77733e461e85f8
arist0v <arist0v@disroot.org>
2022-03-11 09:43:03 -0500

    debug

--- pkg/hq_device.py
+++ pkg/hq_device.py
@@ -112,7 +112,7 @@ class hq_Device(Device):
 
         for property in self.properties:
             if property == 'ActiveEvent':
-                self.find_property(property).update(self.find_property(property).is_active(self.datas.nextEvent, endEvent))
+                self.find_property(property).set_RO_Value(property,self.find_property(property).is_active(self.datas.nextEvent, endEvent))
             elif property == 'PreHeatEvent':
                 self.find_property(property).set_RO_Value(property, self.find_property(property).is_active(preHeatStart, self.datas.nextEvent))
             elif property == 'PostHeatEvent':
