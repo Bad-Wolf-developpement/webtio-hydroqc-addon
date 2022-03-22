6b690d0a3059baf378efa00c2eb26c279bb168e8
Martin Verret <verret.martin@gmail.com>
2022-03-22 08:43:27 -0400

    package.sh update

--- package.sh
+++ package.sh
@@ -1,30 +1,21 @@
 #!/bin/bash -e
-
 version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
-
 # Clean up from previous releases
 rm -rf *.tgz package SHA256SUMS lib
-
 # Prep new package
 mkdir lib package
-
 # Pull down Python dependencies
 pip3 install -r requirements.txt -t lib --prefix ""
-
 # Put package together
 cp -r lib pkg LICENSE manifest.json *.py README.md package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
-
 # Generate checksums
 cd package
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
-
 # Make the tarball
 TARFILE="webtio-hydroqc-addon-${version}.tgz"
 tar czf ${TARFILE} package
-
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
-
 rm -rf SHA256SUMS package lib
