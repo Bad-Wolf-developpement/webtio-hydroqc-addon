6671d573cebcc4d49828933fe05d17295fa2740b
arist0v <arist0v@disroot.org>
2022-04-10 10:55:41 -0400

    add some debuging print

--- pkg/hq_device.py
+++ pkg/hq_device.py
@@ -45,6 +45,7 @@ class hq_Device(Device):
         """         
         if self.adapter.verbose:
             print("updating hq datas")
+            print(self.datas, self.new_datas)
             print("Old Datas: {0}".format(self.datas.lastSync))
             print("New Datas: {0}".format(self.new_datas.lastSync))
         if self.data_changed():
