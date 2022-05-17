e447a4328e42b2a09445d4e45c895739e05ac3c2
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-05-17 13:58:04 +0200

    Update package.sh
    That patching should probably only happen on python 3.7? I've added some commented code for that, which you may want to uncomment and test.

--- package.sh
+++ package.sh
@@ -1,23 +1,57 @@
 #!/bin/bash -e
+
+echo "in package.sh"
+
 version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+
+# Setup environment for building inside Dockerized toolchain
+[ $(id -u) = 0 ] && umask 0
+
 # Clean up from previous releases
-rm -rf *.tgz package SHA256SUMS lib
+echo "removing old files"
+rm -rf *.tgz *.sha256sum package SHA256SUMS lib
+
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+  #PYTHON_VERSION=3.7
+else
+  PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
+
+
 # Prep new package
-mkdir lib package
+echo "creating package"
+mkdir -p lib package
+
 # Pull down Python dependencies
-pip3 install -r requirements.txt -t lib --prefix ""
-#Fix hydroqc compatibility to 3.7
-python patch_hydroqc.py
+pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
+
+# Patch for python 3.7
+#if [ "$PYTHON_VERSION" = "3.7" ]; then
+python3 patch_hydroqc.py
+#fi
+
 # Put package together
-cp -r lib pkg LICENSE manifest.json *.py README.md package/
+cp -r lib pkg LICENSE manifest.json *.py README.md  css images js views  package/
 find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete # Mac OS creates these files
 find package -type d -empty -delete
+rm -rf package/pkg/pycache
+
 # Generate checksums
+echo "generating checksums"
 cd package
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
+
 # Make the tarball
-TARFILE="webtio-hydroqc-addon-${version}.tgz"
+echo "creating archive"
+TARFILE="candleappstore-${version}${TARFILE_SUFFIX}.tgz"
 tar czf ${TARFILE} package
+
+echo "creating tgz shasum"
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
-rm -rf SHA256SUMS package lib
+cat ${TARFILE}.sha256sum
+
+#rm -rf SHA256SUMS package
